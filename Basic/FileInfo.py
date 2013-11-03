95a20af43591c87a4276c8bdb7bfcd4f3365db41
guorui <guorui016@gmail.com>
2013-11-03 17:51:55 +0800

    ok

--- Basic/FileInfo.py
+++ Basic/FileInfo.py
@@ -41,11 +41,11 @@ class MP3FileInfo(FileInfo):
     "store ID3v1.0 MP3 tags"
     tagDataMap = {
         "title": (3, 33, stripnulls),
-        "artist": (3, 33, stripnulls),
-        "album": (3, 33, stripnulls),
-        "year": (3, 33, stripnulls),
-        "comment": (3, 33, stripnulls),
-        "genre": (3, 33, stripnulls)
+        "artist": (33, 63, stripnulls),
+        "album": (63, 93, stripnulls),
+        "year": (93, 97, stripnulls),
+        "comment": (97, 126, stripnulls),
+        "genre": (127, 128, ord)
     }
 
     def __parse(self, filename):
@@ -63,6 +63,28 @@ class MP3FileInfo(FileInfo):
                     self[tag] = parseFunc(tagdata[start:end])
         except IOError:
             pass
+
+    def __setitem__(self, key, item):
+        if key == "name" and item:
+            self.__parse(item)
+        FileInfo.__setitem__(self,key,item)
+
+
+def listDirectory(directory,fileExtList):
+    "get list of file info objects for files of particular extensions"
+    fileList = [os.path.normcase(f) for f in os.listdir(directory)]
+    fileList = [os.path.join(directory, f) for f in fileList \
+                if os.path.splitext(f)[1] in fileExtList]
+    def getFileInfoClass(filename, module=sys.modules[FileInfo.__module__]):
+        "get file info class from filename extensions"
+        subclass = "%sFileInfo" % os.path.splitext(filename)[1].upper()[1:]
+        return hasattr(module, subclass) and getattr(module, subclass) or FileInfo
+    return [getFileInfoClass(f)(f) for f in fileList]
+
+if __name__ == "__main__":
+    for info in listDirectory("/music/_singles/", [".mp3"]):
+        print "\n".join(["%s=%s" % (k, v) for k, v in info.items()])
+        print
 
 
 
